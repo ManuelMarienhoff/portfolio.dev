a51bfcd7e296eb964bb196b9c223c36ce6e1b858
Manuel Marienhoff <manuelmarienhoff1@gmail.com>
2024-08-22 19:07:07 -0300

    add whatsapp button

--- src/TranslateScript.js
+++ src/TranslateScript.js
@@ -5,9 +5,10 @@ const inputEmail = document.querySelector('#email');
 const inputSubject = document.querySelector('#asunto');
 const inputMessage = document.querySelector('#mensaje');
 const sendEmailButton = document.querySelector('#sendEmail');
+const whatsappLinks = document.querySelectorAll('.whatsapp-float')
 
 button.addEventListener('click', () => {
-  /* Traduce a español */
+  /* Traduce a ingles */
   if (button.classList.contains('english')) {
     textSpanish.forEach((element) => {
       element.classList.add('hidden');
@@ -22,9 +23,17 @@ button.addEventListener('click', () => {
     inputMessage.placeholder = 'Message';
     sendEmailButton.value = 'Send';
 
+    whatsappLinks.forEach((link) => {
+      if (link.classList.contains('english')) {
+        link.classList.remove('hidden');
+      } else if (link.classList.contains('spanish')) {
+        link.classList.add('hidden');
+      }
+    });
+
     button.classList.add('spanish');
     button.classList.remove('english');
-    /* Traduce a ingles */
+    /* Traduce a español */
   } else if (button.classList.contains('spanish')) {
     textEnglish.forEach((element) => {
       element.classList.add('hidden');
@@ -38,6 +47,14 @@ button.addEventListener('click', () => {
     inputSubject.placeholder = 'Asunto';
     inputMessage.placeholder = 'Mensaje';
     sendEmailButton.value = 'Enviar';
+
+    whatsappLinks.forEach((link) => {
+      if (link.classList.contains('spanish')) {
+        link.classList.remove('hidden');
+      } else if (link.classList.contains('english')) {
+        link.classList.add('hidden');
+      }
+    });
 
     button.classList.add('english');
     button.classList.remove('hidden');
